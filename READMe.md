f50b341952d11bc86afbc8adadae18502f29f04c
RICKXin9 <1970551651@qq.com>
2023-03-16 19:47:10 +0800

    init repo

---
+++ READMe.md
@@ -0,0 +1,6 @@
+##一个社区的项目
+
+##资料
+https://spring.io/
+https://spring.io/projects/spring-framework
+##工具
---
+++ src/test/java/com/communitystart/communitystart/CommunitystartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communitystart.communitystart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitystartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
